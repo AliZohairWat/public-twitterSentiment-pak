bf8a171278489380416ffc4cd20e9e3c2d99f441
AliZohairWAT <alizohair32@hotmail.com>
2023-08-13 12:07:59 -0400

    updated config_loader

--- config/config_loader.py
+++ config/config_loader.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 import json
 
+root_path = "C:/Users/User/Documents/Ali/Projects/public-twitterSentiment-pak"
+
 def load_config():
-    project_root = Path.cwd()
+    project_root = Path(root_path)
 
     config_path = project_root / "config" / "config.json"
 
@@ -10,5 +12,8 @@ def load_config():
 
     with config_path.open() as cf:
         config = json.load(cf)
+        new_config = {}
+        for path in config:
+            new_config[path] = Path(root_path + config[path])
     
-    return config
+    return new_config
